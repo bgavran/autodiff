c5ad21ec9596203104055f973bccc6d46242e0c0
bgavran3 <bruno.gavranovic@fer.hr>
2017-03-15 17:40:44 +0100

    Refactored code; added meshgrid class

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ class WeirdF(CompositeOperation):
         x0, x1 = self.children[0], self.children[1]
         w0, w1 = self.children[2], self.children[3]
         y = self.children[4]
-        umn0 = w0
-        umn1 = w1
+        umn0 = x0 * w0
+        umn1 = x1 * w1
         sigm0 = Sigmoid([umn0])
-        sigm1 = Sigmoid([umn1])
-        self.out = sigm0*sigm1
+        umn = sigm0 * umn1
+        cost = SquareCost([umn, y])
+        self.out = cost
 
 
 with_respect_to = ["w0", "w1"]
@@ -21,22 +22,11 @@ inp = Variable(name="x0"), Variable(name="x1"), \
 
 graph = WeirdF(inp, "comp_graph")
 
-###
-mx = create_meshgrid(x_len, xmax, xn_points)
-mw = create_meshgrid(len(with_respect_to), wmax, wn_points)
-
-# rearanging the array, based on the wrt argument, should work for 3 dimensions also
-myorder = [int(i[1]) for i in with_respect_to]
-mw_input = [mw[i] for i in myorder]
-###
-
-dw_grad = np.zeros((len(with_respect_to), wn_points, wn_points))
-grad = sum_function_outputs(graph.gradient_list, mx, mw_input, dw_grad, with_respect_to)
-
-dw_val = np.zeros((wn_points, wn_points))
-val = sum_function_outputs(graph.f, mx, mw_input, dw_val)
+m = GraphMeshgrid(with_respect_to)
+grad = m.apply_to_function(graph.gradient_list, with_respect_to)
+val = m.apply_to_function(graph.f)
 
 p = Plotter()
-p.plot_stream(mw, grad, with_respect_to)
-p.plot_value(mw_input[0], mw_input[1], val, with_respect_to)
+p.plot_stream(m.w, grad, with_respect_to)
+p.plot_value(m.w[0], m.w[1], val, with_respect_to)
 plt.show(block=True)
--- utils.py
+++ utils.py
@@ -7,24 +7,33 @@ x_len = 2  # number (dimension) of input
 xmax = 5
 xn_points = 2 ** p_exp
 
-wmax = 5
+wmax = 2
 wn_points = 2 ** p_exp
+
+
+class GraphMeshgrid:
+    def __init__(self, with_respect_to):
+        self.x = create_meshgrid(x_len, xmax, xn_points)
+        self.w = create_meshgrid(len(with_respect_to), wmax, wn_points)
+
+        # rearanging the array, based on the wrt argument, should work for 3 dimensions also
+        myorder = [int(i[1]) for i in with_respect_to]
+        # ow = ordered weight
+        self.ow = [self.w[i] for i in myorder]
+
+    def apply_to_function(self, graph_function, *fun_args):
+        # iterating through every input, producing the output and summing the gradients
+        l = list(np.nditer(self.x))
+        all_x = np.array(
+            [graph_function({"x0": x[0], "x1": x[1], "w0": self.ow[0], "w1": self.ow[1], "y": if_func(x[0], x[1], 1)},
+                            *fun_args)
+             for x in
+             tqdm(l)])
+
+        return np.sum(all_x, axis=0) / len(l)
 
 
 def create_meshgrid(lenn, maxx, points):
     # add offsetting with x_d, y_d and z_d?
     step = 2 * maxx / points
     return np.meshgrid(*(lenn * [np.arange(-maxx, maxx, step)]))
-
-
-def sum_function_outputs(graph_fun, mx, mw, dw, *fun_args):
-    # iterating through every input, producing the output and summing the gradients
-    l = list(np.nditer(mx))
-    for x in tqdm(l):
-        y = if_func(x[0], x[1], 1)
-        input_dict = {"x0": x[0], "x1": x[1], "w0": mw[0], "w1": mw[1], "y": y}
-
-        dw += graph_fun(input_dict, *fun_args)
-
-    # start point (uniform distribution), end point (calculated gradient)
-    return dw / mx[0].size
