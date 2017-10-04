Fixed softmax gradient, other minor stuff

src/core/ops.py
@@ -108,7 +108,8 @@ class Einsum(Primitive):
         self.opnames = re.split(",|->", self.op_str)
         self.all_letters = "".join(set("".join(self.opnames[:-1])))
 
-        assert len(self.operands) + 1 == len(self.opnames)
+        if len(self.operands) + 1 != len(self.opnames):
+            raise ValueError("Number of operands doesn't match the einsum string!")
 
     def f(self):
         """
@@ -126,7 +127,9 @@ class Einsum(Primitive):
             if isinstance(val, np.ndarray):
                 shape = val.shape
                 letters = self.opnames[i]
-                assert len(shape) == len(letters)
+                if len(shape) != len(letters) and "..." not in letters:  # ellipsis check needs to be solved properly!
+                    raise ValueError("Dimension of operand " + str(i + 1) + " doesn't match the string! " +
+                                     "Shape: " + str(shape) + " , string: '" + letters + "'")
                 for letter, dim in zip(letters, shape):
                     letter_to_dim[letter] = dim
 
@@ -165,14 +168,14 @@ class Einsum(Primitive):
 
             # add explicit Variables for implicitly summed out tensors
             from core.reshape import Shape
-            wrt_shape = Shape(wrt)
+
+            wrt_shape = Shape(wrt)()
             for i, letter in enumerate(self.opnames[loc]):
                 if letter not in "".join(opnames[:-1]):
                     opnames.insert(0, letter)
 
                     dim = wrt_shape[i]
-                    # we're running here dim()!!! that shouldn't be done?
-                    var_to_insert = Variable(np.ones(dim()))
+                    var_to_insert = Variable(np.ones(dim))
                     operands_with_grad.insert(0, var_to_insert)
             op_str = Einsum.to_einsum_string(opnames)
 
@@ -203,51 +206,35 @@ class ReLU(Primitive):
         return 0
 
 
-class Equal(Primitive):
-    def __init__(self, first, second, name="Equal"):
-        super().__init__([first, second], name)
-        self.first, self.second = self.children
-
-    def f(self):
-        return np.equal(self.first(), self.second())
-
-    def graph_df(self, wrt, grad):
-        # is this correct?
-        return 0
-
-
 class Softmax(Module):
     """
-    Softmax is a vector function: R^n -> R^n and taking its partial derivative is a bit tricky?
-    We have a jacobian instead of a gradient?
-    Interesting property, softmax is not invariant to scaling?
+    Softmax is a vector function: R^n -> R^n and taking its partial derivative w.r.t. is a jacobian.
+    But we sum the jacobian along the output axis?
 
     """
 
-    def __init__(self, node, ind, name="Softmax", expand_graph=True):
+    def __init__(self, node, name="Softmax", expand_graph=True):
         super().__init__([node], name, expand_graph=expand_graph)
         self.node = self.children[0]
-        self.ind = ind  # index of the element whose Softmax we want to compute
         self.out = self.init_graph()
 
     def graph(self):
-        exp_ind = Exp(self.node[self.ind])
-        exp_all = Exp(self.node)
-        return exp_ind / Einsum("j->", exp_all)
+        exp = Exp(self.node)
+        one = Variable(np.array([1]))
+        return exp / Einsum("...j,o->...o", exp, one)
 
     @module_wrapper
     def graph_df(self, wrt, grad):
         # gradient depends on every ind in self.node() ?
         if wrt == self.node:
-            arr = []
-            for dim in range(self.node().shape[-1]):
-                if dim == self.ind:
-                    val = self * (1 - self)
-                else:
-                    val = - self * Softmax(self.node, dim)
-                arr.append(val)
-            arr = np.array([op() for op in arr])
-            return grad * Variable(arr)
+            # matrix of the self outer product
+            outer = Einsum("...i,...j->...ij", self, self)
+
+            ones_diag = Variable(np.eye(self().shape[-1]))
+            # matrix where the only nonzero elements is the softmax on the diagonal
+            kronecker_val = Einsum("...ij,...j->...ij", ones_diag, self)
+
+            return grad * Einsum("...ij->...j", outer - kronecker_val)
         return 0
 
 

src/main.py
@@ -5,15 +5,14 @@ np.random.seed(1337)
 x_val = np.random.rand(7)
 x = Variable(x_val, name="x")
 
-graph = Softmax(x, 2)
 print(x())
+
+graph = Softmax(x)
+print("------------")
 print(graph())
 
 graph = Grad(graph, x)
-
+print("------------")
 print(graph())
-
-# graph = Grad(graph, x, expand_graph=True)
-# print(graph())
 
 plot_comp_graph(graph, view=False)

src/neural_network.py
@@ -7,15 +7,15 @@ np.random.seed(1337)
 
 # (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+
 # flatten the data?
-# first fix einsum?
 
 @checkpoint
 def nn(x, w0):
     print("steppp")
 
-    x_reshaped = Reshape(x, Variable(np.random.randn(28 * 28)))
-    graph = Sigmoid(x_reshaped @ w0, expand_graph=False)
+    x_reshaped = Reshape(x, Shape(from_tuple=(-1, 28 * 28)))  # [batch_size, 28*28]
+    graph = Softmax(x_reshaped @ w0)  # [batch_size, 10]
     return graph
 
 
@@ -32,14 +32,12 @@ def update_weight(out, w):
 
 
 def get_data():
-    return np.random.randn(28, 28), np.random.randint(0, 10)
+    batch_size = 3
+    return np.random.randn(batch_size, 28, 28), np.random.randint(batch_size, 10)
 
 
-w0_val = np.random.randn(28 * 28, 10)
-w1_val = np.random.randn(5, 7)
-
-w0 = Variable(w0_val, name="w0")
-w1 = Variable(w1_val, name="w1")
+w0 = Variable(np.random.randn(28 * 28, 10), name="w0")  # [batch_size, 28, 28]
+w1 = Variable(np.random.randn(5, 7), name="w1")  # [batch_size, 28*28, 10]
 
 n_steps = 1000
 for i in range(n_steps):

tests/test_one_arg_operations.py
@@ -10,6 +10,10 @@ from tests import utils
 
 class TestOneArgOperations(TestCase):
     def setUp(self):
+        """
+        Just basic testing, needs to be properly expanded
+
+        """
         np.random.seed(1337)
         self.w0_val = np.random.randn(2, 3)
         self.w1_val = np.random.rand(7)
@@ -23,9 +27,8 @@ class TestOneArgOperations(TestCase):
         self.n_times = 3
 
     def test_softmax(self):
-        ind = 2
-        my_graph = Softmax(self.my_w1, ind)
-        tf_graph = tf.nn.softmax(self.tf_w1)[ind]
+        my_graph = Softmax(self.my_w1)
+        tf_graph = tf.nn.softmax(self.tf_w1)
         utils.test_one_op(self, my_graph, tf_graph, [self.my_w1], [self.tf_w1], n=self.n_times)
 
     def test_reshape(self):