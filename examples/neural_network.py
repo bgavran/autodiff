d8b20bb64dee92557b49be434ef6007c7acd9c9d
bgavran3 <bruno.gavranovic@fer.hr>
2017-10-18 10:22:14 +0200

    Added softmax_ce_logits to the example

--- examples/neural_network.py
+++ examples/neural_network.py
@@ -7,14 +7,12 @@ from examples.data import get_data
 @ad.module_wrapper
 def nn(x, w0):
     x_reshaped = ad.Reshape(x, ad.Shape(from_tuple=(-1, 28 * 28)))  # [batch_size, 28*28]
-    # TODO Add working softmax here!
-    graph = ad.Softmax(x_reshaped @ w0)  # [batch_size, 10]
-    return graph
+    return x_reshaped @ w0
 
 
 @ad.module_wrapper
-def loss(nn_output, y):
-    return ad.SquaredDifference(nn_output, y)
+def softmax_ce_logits(labels, logits):
+    return ad.Einsum("i->", ad.SoftmaxCEWithLogits(labels=labels, logits=logits))
 
 
 @ad.module_wrapper
@@ -25,29 +23,30 @@ def optimizer(w, w_grad):
 
 w0 = ad.Variable(np.random.randn(28 * 28, 10) * 0.01, name="w0")
 
-for i in range(1000):
+for i in range(2000):
     x_val, y_val = get_data(train=True)
     x, y = ad.Variable(x_val, name="x"), ad.Variable(y_val, name="y")
 
-    network = nn(x, w0)
-    loss_var = loss(network, y)
+    logits = nn(x, w0)
 
-    w0_grad = ad.Grad(loss_var, w0)
+    loss = softmax_ce_logits(y, logits)
+
+    w0_grad = ad.Grad(loss, w0)
     new_w0 = optimizer(w0, w0_grad)
 
     w0 = ad.Variable(new_w0(), name="w0")
 
     if i % 100 == 0:
         print("Step:", i)
-        print("Loss:", np.sum(loss_var()))
+        print("Loss:", np.sum(loss()))
         print("-----------")
 
 print("Testing...")
 x, y = get_data(train=False)
 x, y = ad.Variable(x, name="x"), ad.Variable(y, name="y")
 
-test_network = nn(x, w0)
-test_loss = loss(test_network, y)
+test_network = ad.Softmax(nn(x, w0))
+test_loss = softmax_ce_logits(y, nn(x, w0))
 
 true = np.argmax(y(), -1)
 pred = np.argmax(test_network(), -1)
