fa8cb7faa2fe93d95125350c1b4d62b57e6a0a7b
bgavran3 <bruno.gavranovic@fer.hr>
2017-10-25 14:31:19 +0200

    Added sane importing possibilities

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 from automatic_differentiation.src.core.computational_graph import *
 from automatic_differentiation.src.core.ops import *
 from automatic_differentiation.src.core.reshape import *
+from automatic_differentiation.src.core.high_level_ops import *
+from automatic_differentiation.src.core.wrappers import *
 
 from automatic_differentiation.src.visualization.graph_visualization import plot_comp_graph
