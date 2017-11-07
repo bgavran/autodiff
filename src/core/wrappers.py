75acd1324926449bcea991024f7ec4be74c135f3
bgavran3 <bruno.gavranovic@fer.hr>
2017-11-07 22:04:18 +0100

    renamed variables

--- src/core/wrappers.py
+++ src/core/wrappers.py
@@ -5,8 +5,8 @@ def checkpoint(fn):
     def wrap_in_primitive(*fn_args):
         op = ad.Primitive(children=fn_args, name=fn.__name__)
 
-        op.f = lambda: fn(*fn_args)()
-        op.graph_df = lambda wrt, curr_grad: ad.grad(fn(*fn_args), [wrt], curr_grad=curr_grad)[0]
+        op._eval = lambda: fn(*fn_args)()
+        op.partial_derivative = lambda wrt, previous_grad: ad.grad(fn(*fn_args), [wrt], previous_grad=previous_grad)[0]
         # should graph_df return the already called Node() or just the Node, like right now?
 
         return op
