d22dee50108db10b1fed43f0f011bc8e8ca8f7b9
bgavran3 <bruno.gavranovic@fer.hr>
2017-11-21 16:12:17 +0100

    refactored imports, changed testing precision

--- tests/numerical_check.py
+++ tests/numerical_check.py
@@ -1,6 +1,6 @@
 import numpy as np
-import timeout_decorator
-import automatic_differentiation as ad
+from automatic_differentiation.src.core.grad import grad
+from automatic_differentiation.src.core.ops import Exp
 
 
 def uncache_subtree(node):
@@ -36,16 +36,16 @@ def numerical_gradient(graph, wrt_var, eps=1e-6):
 
 def differentiate_n_times_num(graph, wrt_vars, order):
     if order == 1:
-        backprop_graphs = ad.grad(graph, wrt_vars)
+        backprop_graphs = grad(graph, wrt_vars)
         numeric_grads = [numerical_gradient(graph, var) for var in wrt_vars]
     else:
         for o in range(order):
             if o == 0:
-                backprop_graphs = ad.grad(graph, wrt_vars)
+                backprop_graphs = grad(graph, wrt_vars)
             else:
                 if o == order - 1:
                     numeric_grads = [numerical_gradient(graph, var) for graph, var in zip(backprop_graphs, wrt_vars)]
-                backprop_graphs = [ad.grad(graph, [var])[0] for graph, var in zip(backprop_graphs, wrt_vars)]
+                backprop_graphs = [grad(graph, [var])[0] for graph, var in zip(backprop_graphs, wrt_vars)]
     return backprop_graphs, numeric_grads
 
 
@@ -61,13 +61,17 @@ def numerical_check(test, graph, wrt_vars, order=1):
             print("Backprop grad:", graph_grad())
             print("Numeric grad:", num_grad)
             broadcasted_grad = np.broadcast_to(graph_grad(), wrt_var().shape)  # not necessarily the same shape
-            np.testing.assert_allclose(broadcasted_grad, num_grad, rtol=1e-2, atol=1e-6)
+            arrays_allclose(broadcasted_grad, num_grad)
+
+
+def arrays_allclose(a, b):
+    np.testing.assert_allclose(a, b, rtol=1e-2, atol=1e-5)
 
 
 def test_numeric(test, graph, wrt_vars, order=1):
     for deriv in range(1, order + 1):
         numerical_check(test, graph, wrt_vars, order=deriv)
 
-    extended_graph = ad.Exp(graph, name="extra_exp_op")
+    extended_graph = Exp(graph, name="extra_exp_op")
     for deriv in range(1, order + 1):
         numerical_check(test, extended_graph, wrt_vars, order=deriv)
--- tests/utils.py
+++ tests/utils.py
@@ -1,7 +1,8 @@
 import numpy as np
 import tensorflow as tf
-import timeout_decorator
-import automatic_differentiation as ad
+from automatic_differentiation.src.core.grad import grad
+from automatic_differentiation.src.core.ops import Exp
+from automatic_differentiation.tests.numerical_check import test_numeric
 
 
 def differentiate_n_times(my_graph, tf_graph, my_vars, tf_vars, order=1, my_curr_grad=None, tf_curr_grad=None):
@@ -10,12 +11,12 @@ def differentiate_n_times(my_graph, tf_graph, my_vars, tf_vars, order=1, my_curr
 
     for i in range(order):
         if i == 0:
-            my_graphs = ad.grad(my_graph, my_vars, previous_grad=my_curr_grad)
+            my_graphs = grad(my_graph, my_vars, previous_grad=my_curr_grad)
             if tf_graph is not None:
                 tf_graphs = tf.gradients(tf_graph, tf_vars, grad_ys=tf_curr_grad)
         else:
             for i in range(len(my_graphs)):
-                my_graphs[i] = ad.grad(my_graphs[i], [my_vars[i]])[0]
+                my_graphs[i] = grad(my_graphs[i], [my_vars[i]])[0]
                 if tf_graphs[i] is not None:
                     tf_graphs[i] = tf.gradients(tf_graphs[i], tf_vars[i])[0]
     return my_graphs, tf_graphs
@@ -32,7 +33,7 @@ def eval_graphs(my_graph, tf_graph, my_var, tf_var, n):
     print("Tf_val:", tf_grads)
     my_val = np.broadcast_to(my_grads, my_var.shape)
     tf_val = np.broadcast_to(tf_grads, my_var.shape)
-    np.testing.assert_allclose(my_val, tf_val, rtol=1e-2, atol=1e-6)
+    arrays_allclose(my_val, tf_val)
 
 
 def oneop_df_n_times(test, my_graph, tf_graph, my_vars, tf_vars, n=1, my_curr_grad=None, tf_curr_grad=None):
@@ -54,7 +55,7 @@ def oneop_f(my_graph, tf_graph):
     print("---------- f ----------")
     print("My_val:", my_val)
     print("Tf_val:", tf_val)
-    np.testing.assert_allclose(my_val, tf_val)
+    arrays_allclose(my_val, tf_val)
 
 
 def test_one_op(test, my_graph, tf_graph, my_wrt, tf_wrt, order=1):
@@ -73,12 +74,16 @@ def test_one_op(test, my_graph, tf_graph, my_wrt, tf_wrt, order=1):
         print("    with random curr_grad    ")
         print("#############################")
 
-        my_extended_graph = ad.Exp(my_graph, name="extra_exp_op")
+        my_extended_graph = Exp(my_graph, name="extra_exp_op")
         tf_extended_graph = tf.exp(tf_graph, name="extra_exp_op")
         for deriv in range(1, order + 1):
             oneop_df_n_times(test, my_extended_graph, tf_extended_graph, my_wrt, tf_wrt, n=deriv)
 
 
-def custom_test(test, my_graph, my_wrt_vars, tf_graph=None, tf_wrt_vars=None):
-    # test_one_op(test, my_graph, tf_graph, my_wrt_vars, tf_wrt_vars)
-    ad.test_numeric(test, my_graph, my_wrt_vars)
+def arrays_allclose(a, b):
+    np.testing.assert_allclose(a, b, rtol=1e-2, atol=1e-5)
+
+
+def custom_test(test, my_graph, my_wrt_vars, tf_graph=None, tf_wrt_vars=None, order=1):
+    # test_one_op(test, my_graph, tf_graph, my_wrt_vars, tf_wrt_vars, order=order)
+    test_numeric(test, my_graph, my_wrt_vars, order=order)
