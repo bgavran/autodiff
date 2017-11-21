447c57771fa43a5cc9d8d906ea30212a375a80b3
bgavran3 <bruno.gavranovic@fer.hr>
2017-11-21 16:11:38 +0100

    refactored imports

--- src/core/wrappers.py
+++ src/core/wrappers.py
@@ -1,14 +1,17 @@
-import automatic_differentiation as ad
+from contextlib import contextmanager
+from automatic_differentiation.src.core.computational_graph import Node, Primitive
+from automatic_differentiation.src.core.grad import grad
 
 
 def checkpoint(fn):
     def wrap_in_primitive(*fn_args):
-        op = ad.Primitive(children=fn_args, name=fn.__name__)
+        op = Primitive(children=fn_args, name=fn.__name__)
 
         op._eval = lambda: fn(*fn_args)()
-        op.partial_derivative = lambda wrt, previous_grad: ad.grad(fn(*fn_args), [wrt], previous_grad=previous_grad)[0]
+        op._partial_derivative = lambda wrt, previous_grad: grad(fn(*fn_args), [wrt], previous_grad=previous_grad)[0]
         # should graph_df return the already called Node() or just the Node, like right now?
 
         return op
 
     return wrap_in_primitive
+
