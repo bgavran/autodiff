005e3599f9b0c56febf14f9e33aff0e611e3c152
bgavran3 <bruno.gavranovic@fer.hr>
2017-11-21 16:07:22 +0100

    refactored imports

--- __init__.py
+++ __init__.py
@@ -1,9 +1,12 @@
-from automatic_differentiation.src.core.computational_graph import *
+from automatic_differentiation.src.core.computational_graph import Node, Variable, Primitive
 from automatic_differentiation.src.core.ops import *
 from automatic_differentiation.src.core.reshape import *
 from automatic_differentiation.src.core.high_level_ops import *
 from automatic_differentiation.src.core.wrappers import *
 
 from automatic_differentiation.src.visualization.graph_visualization import plot_comp_graph
+
 from automatic_differentiation.tests.utils import *
 from automatic_differentiation.tests.numerical_check import *
+
+from automatic_differentiation.examples.pytorch_data_loader import *
